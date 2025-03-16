b0cec17d8920b78aed2133d32e3a4283c642e7be
Toint <599818663@qq.com>
2025-03-17 01:00:05 +0800

    add JdyHttpUtil

--- toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/service/impl/JdyRequestServiceImpl.java
+++ toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/service/impl/JdyRequestServiceImpl.java
@@ -3,6 +3,7 @@ package cn.toint.jdy4j.core.service.impl;
 import cn.toint.jdy4j.core.model.JdyConfigStorage;
 import cn.toint.jdy4j.core.service.JdyConfigStorageService;
 import cn.toint.jdy4j.core.service.JdyRequestService;
+import cn.toint.jdy4j.core.util.JdyHttpUtil;
 import cn.toint.tool.json.JacksonUtil;
 import cn.toint.jdy4j.core.util.JdyConfigStorageHolder;
 import cn.toint.jdy4j.core.util.JdyUrlUtil;
@@ -13,12 +14,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.dromara.hutool.core.lang.Assert;
 import org.dromara.hutool.core.text.StrUtil;
 import org.dromara.hutool.core.thread.RetryableTask;
-import org.dromara.hutool.http.client.ClientConfig;
 import org.dromara.hutool.http.client.Request;
 import org.dromara.hutool.http.client.Response;
-import org.dromara.hutool.http.client.engine.ClientEngine;
-import org.dromara.hutool.http.client.engine.ClientEngineFactory;
-import org.dromara.hutool.http.client.engine.okhttp.OkHttpEngine;
 import org.dromara.hutool.http.meta.HeaderName;
 
 import java.time.Duration;
@@ -34,13 +31,6 @@ public class JdyRequestServiceImpl implements JdyRequestService {
      */
     @Resource
     private JdyConfigStorageService jdyConfigStorageService;
-
-    /**
-     * 简道云默认 http 客户端
-     * 服务端3秒内未响应第一次内容则超时报错
-     */
-    private final ClientEngine clientEngine = ClientEngineFactory.createEngine(OkHttpEngine.class.getName())
-            .init(ClientConfig.of().setTimeout(3000));
 
     @Override
     public JsonNode request(final Request request) {
@@ -73,7 +63,7 @@ public class JdyRequestServiceImpl implements JdyRequestService {
     }
 
     private JsonNode doRequest(final Request request) {
-        try (final Response response = request.send(clientEngine)) {
+        try (final Response response = request.send(JdyHttpUtil.getClientEngine())) {
             final String bodyStr = response.bodyStr();
             if (!response.isOk() || StringUtils.isBlank(bodyStr)) {
                 final String msg = StrUtil.format("请求简道云服务器报错, status: {}, resBody: {}", response.getStatus(), bodyStr);
---
+++ toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/util/JdyHttpUtil.java
@@ -0,0 +1,29 @@
+package cn.toint.jdy4j.core.util;
+
+import org.dromara.hutool.http.client.ClientConfig;
+import org.dromara.hutool.http.client.Request;
+import org.dromara.hutool.http.client.Response;
+import org.dromara.hutool.http.client.engine.ClientEngine;
+import org.dromara.hutool.http.client.engine.ClientEngineFactory;
+import org.dromara.hutool.http.client.engine.okhttp.OkHttpEngine;
+
+/**
+ * @author Toint
+ * @date 2025/3/17
+ */
+public class JdyHttpUtil {
+    /**
+     * 简道云默认 http 客户端
+     * 服务端3秒内未响应第一次内容则超时报错
+     */
+    private static final ClientEngine CLIENT_ENGINE = ClientEngineFactory.createEngine(OkHttpEngine.class.getName())
+            .init(ClientConfig.of().setTimeout(3000));
+
+    public static ClientEngine getClientEngine() {
+        return JdyHttpUtil.CLIENT_ENGINE;
+    }
+
+    public static Response request(final Request request) {
+        return JdyHttpUtil.CLIENT_ENGINE.send(request);
+    }
+}
