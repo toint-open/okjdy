7789aa5259207d855a955f5c6a3af54838f97306
Toint <599818663@qq.com>
2025-05-29 23:59:59 +0800

    v1.1.3

--- toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/util/JdyHttpUtil.java
+++ toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/util/JdyHttpUtil.java
@@ -16,13 +16,13 @@
 
 package cn.toint.jdy4j.core.util;
 
+import cn.toint.tool.util.HttpClientUtil;
 import cn.toint.tool.util.JacksonUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.dromara.hutool.http.client.ClientConfig;
 import org.dromara.hutool.http.client.Request;
 import org.dromara.hutool.http.client.Response;
 import org.dromara.hutool.http.client.engine.ClientEngine;
-import org.dromara.hutool.http.client.engine.ClientEngineFactory;
 import org.dromara.hutool.http.client.engine.okhttp.OkHttpEngine;
 
 import java.util.Optional;
@@ -36,8 +36,7 @@ public class JdyHttpUtil {
      * 简道云默认 http 客户端
      * 服务端10秒内未响应第一次内容则超时报错
      */
-    private static final ClientEngine CLIENT_ENGINE = ClientEngineFactory.createEngine(OkHttpEngine.class.getName())
-            .init(ClientConfig.of().setTimeout(10000));
+    private static final ClientEngine CLIENT_ENGINE = HttpClientUtil.clientEngine(OkHttpEngine.class, ClientConfig.of().setTimeout(10000));
 
     public static ClientEngine getClientEngine() {
         return JdyHttpUtil.CLIENT_ENGINE;
