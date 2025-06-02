adca02fd46739530ea9f5335618d5b48d8fc559b
Toint <599818663@qq.com>
2025-06-02 08:32:49 +0800

    增加关联数据和聚合计算字段类型

--- toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/enums/JdyFieldTypeEnum.java
+++ toint-jdy4j-core/src/main/java/cn/toint/jdy4j/core/enums/JdyFieldTypeEnum.java
@@ -27,27 +27,120 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JdyFieldTypeEnum {
+    /**
+     * 文本
+     */
     TEXT("text"),
+
+    /**
+     * 多行文本
+     */
     TEXT_AREA("textarea"),
+
+    /**
+     * 流水号
+     */
     SN("sn"),
+
+    /**
+     * 数字
+     */
     NUMBER("number"),
+
+    /**
+     * 日期时间
+     */
     DATE_TIME("datetime"),
+
+    /**
+     * 单选按钮组
+     */
     RADIO_GROUP("radiogroup"),
+
+    /**
+     * 复选框组
+     */
     CHECK_BOX_GROUP("checkboxgroup"),
+
+    /**
+     * 下拉框
+     */
     COMBO("combo"),
+
+    /**
+     * 下拉复选框
+     */
     COMBO_CHECK("combocheck"),
+
+    /**
+     * 地址
+     */
     ADDRESS("address"),
+
+    /**
+     * 定位
+     */
     LOCATION("location"),
+
+    /**
+     * 图片
+     */
     IMAGE("image"),
+
+    /**
+     * 附件
+     */
     UPLOAD("upload"),
+
+    /**
+     * 子表单
+     */
     SUBFORM("subform"),
+
+    /*
+    选择数据
+     */
     LINK_DATA("linkdata"),
+
+    /**
+     * 手写签名
+     */
     SIGNA_TURE("signature"),
+
+    /**
+     * 成员单选
+     */
     USER("user"),
+
+    /**
+     * 成员多选
+     */
     USERG_ROUP("usergroup"),
+
+    /**
+     * 部门单选
+     */
     DEPT("dept"),
+
+    /**
+     * 部门多选
+     */
     DEPT_GROUP("deptgroup"),
-    PHONE("phone");
+
+    /**
+     * 手机
+     */
+    PHONE("phone"),
+
+    /**
+     * 关联数据
+     */
+    LOOKUP("lookup"),
+
+    /**
+     * 聚合计算
+     */
+    AGGREGATION("aggregation");
 
     private final String value;
 }
